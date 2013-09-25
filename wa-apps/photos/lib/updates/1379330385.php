e5a70e52b67294fa99b1c952daffd2f87290c7dc
Alexander Muzychenko <alexmuz@gmail.com>
2013-09-25 18:52:01 +0400

    photos bug fix

--- wa-apps/photos/lib/updates/1379330385.php
+++ wa-apps/photos/lib/updates/1379330385.php
@@ -2,7 +2,7 @@
 
 $model = new waModel();
 try {
-    $model->query("SELECT source FROM photos_photos WHERE 0");
+    $model->query("SELECT source FROM photos_photo WHERE 0");
 } catch (waException $e) {
     $model->exec("ALTER TABLE photos_photo ADD COLUMN source VARCHAR(32) NOT NULL DEFAULT 'backend'");
 }
