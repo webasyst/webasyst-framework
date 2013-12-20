4b96a12e01972b6241a532c4ddaf6e304a40a954
Alexander Muzychenko <alexmuz@gmail.com>
2013-12-20 14:38:43 +0400

    site theme VK

--- wa-apps/site/themes/vk/vk.js
+++ wa-apps/site/themes/vk/vk.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         // API initialization succeeded 
         VK.callMethod("setTitle", document.title);
         VK.callMethod("resizeWindow", $("body").outerWidth(), $("body").outerHeight());
+        VK.callMethod("scrollWindow", 0);
     });
     
     $('input[type="button"]').addClass('vk-button');
