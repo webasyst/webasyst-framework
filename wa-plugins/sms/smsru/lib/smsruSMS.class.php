6fd7a8ac09698b0789f328ce4a9de1acc0456084
Alexander Muzychenko <alexmuz@gmail.com>
2013-12-20 15:02:07 +0400

    plugin smsru

--- wa-plugins/sms/smsru/lib/smsruSMS.class.php
+++ wa-plugins/sms/smsru/lib/smsruSMS.class.php
@@ -6,8 +6,8 @@ class smsruSMS extends waSMSAdapter
     {
         return array(
             'api_id' => array(
-                'value' => '',
-                'title' => 'api_id',
+                'value'       => '',
+                'title'       => 'api_id',
                 'description' => 'Введите значение параметра api_id для вашего аккаунта в сервисе sms.ru',
             ),
         );
@@ -18,17 +18,28 @@ class smsruSMS extends waSMSAdapter
      * @param string $text
      * @return mixed
      */
-    public function send($to, $text)
+    public function send($to, $text, $from = null)
     {
+        if (!extension_loaded('curl') || !function_exists('curl_init')) {
+            $this->log($to, $text, "PHP extension curl required");
+            return false;
+        }
         $ch = curl_init("http://sms.ru/sms/send");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-            "api_id"    =>  $this->getOption('api_id'),
-            "to"    =>  $to,
-            "text"  =>  $text
 
-        ));
+        $post = array(
+            "api_id" => $this->getOption('api_id'),
+            "to"     => $to,
+            "text"   => $text
+        );
+        if ($this->getOption('from')) {
+            $post['from'] = $this->getOption('from');
+        } elseif ($from) {
+            $post['from'] = $from;
+        }
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
         $result = curl_exec($ch);
         curl_close($ch);
 
