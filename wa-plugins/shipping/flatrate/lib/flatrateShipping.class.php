6ce3c04c81eb56a31d5be91f4cc3fa7e3db84d08
Alexander Muzychenko <alexmuz@gmail.com>
2014-07-31 15:43:24 +0400

    Shipping plugin Flatrate

--- wa-plugins/shipping/flatrate/lib/flatrateShipping.class.php
+++ wa-plugins/shipping/flatrate/lib/flatrateShipping.class.php
@@ -1,115 +1,145 @@
 <?php
 
 /**
+ * Flat-rate shipping plugin.
  *
  * @property-read float $cost
  * @property-read string $currency
  * @property-read string $delivery
- * @property-read string $prompt_address
+ * @property-read bool $prompt_address
  *
  */
 class flatrateShipping extends waShipping
 {
     /**
-     * Core shipping rate calculation method.
-     * Returns the list (array) of estimated shipping rates and transit times
+     * Main shipping rate calculation method.
+     * Returns array of estimated shipping rates and transit times.
+     * or error message to be displayed to customer,
+     * or false, if this shipping option must not be available under certain conditions.
+     * 
+     * @example <pre>
+     * //return array of shipping options
+     * return array(
+     *     'option_id_1' => array(
+     *          'name'         => $this->_w('...'),
+     *          'description'  => $this->_w('...'),
+     *          'est_delivery' => '...',
+     *          'currency'     => $this->currency,
+     *          'rate'         => $this->cost,
+     *      ),
+     *      ...
+     * );
+     * 
+     * //return error message
+     * return 'Для расчета стоимости доставки укажите регион доставки';
+     * 
+     * //shipping option is unavailable
+     * return false;</pre>
      *
-     * Useful parent class (waShipping) methods to be used in calculate():
-     * $price = $this->getTotalPrice();
-     * $weight = $this->getTotalWeight();
+     * Useful parent class (waShipping) methods to be used in calculate() method:
+     * 
+     *     <pre>
+     *     // total package price
+     *     $price = $this->getTotalPrice();
+     *     
+     *     // total package weight
+     *     $weight = $this->getTotalWeight();
+     *     
+     *     // order items array
+     *     $items = $this->getItems();
+     *     
+     *     // obtain either full address info array or specified address field value
+     *     $address = $this->getAddress($field = null);</pre>
+     *     
+     * @return mixed
      */
     public function calculate()
     {
-        /*
-         Use following methods to obtain package information:
-
-         — $this->getTotalWeight(); // returns overall package weight
-         — $this->getTotalPrice(); // returns declared shipment value
-         — $this->getRecipientName(); // returns full name of the package recipient
-         — $this->getAddress($field = null); // returns either entire address (array) or exact address field, e.g. 'country', 'region', 'city', 'zip', 'street' or any custom defined field
-
-         Use $this->VAR_ID to access module settings as defined in the config/settings.php
-         */
-
+        if ($this->delivery === '') {
+            $est_delivery = null;
+        } else {
+            $est_delivery = waDateTime::format('humandate', strtotime($this->delivery));
+        }
         return array(
             'ground' => array(
                 'name'         => $this->_w('Ground shipping'),
                 'description'  => '',
-                'est_delivery' => ($this->delivery ==='')?null:waDateTime::format('humandate', strtotime($this->delivery)), //string
+                'est_delivery' => $est_delivery, //string
                 'currency'     => $this->currency,
                 'rate'         => $this->cost,
             ),
-            /*
-             //ADD AS MANY SHIPPING OPTIONS AS YOU LIKE
-             'priority' => array(
-             'name' => 'Priority shipping',
-             'description' => '',
-             'estimated_delivery_date' => strtotime($this->delivery),
-             'currency' => $this->currency,
-             'rate' => $this->cost,
-             ),
-             'expedited' => array(
-             'name' => 'Expedited shipping',
-             'description' => '',
-             'estimated_delivery_date' => strtotime($this->delivery),
-             'currency' => $this->currency,
-             'rate' => $this->cost,
-             ),
-             */
         );
     }
 
     /**
-     * Returns ISO3 code of the currency this module can work with (or array of ISO3 codes)
+     * Returns ISO3 code of the currency (or array of ISO3 codes) this plugin supports.
+     *
      * @see waShipping::allowedCurrency()
+     * @return array|string
      */
     public function allowedCurrency()
     {
-        return $this->currency; // return array('USD','EUR');
+        // return array('USD', 'EUR') or simple string: 'USD'
+        return $this->currency;
 
     }
 
     /**
-     * Returns the weight unit this module work with (or array of weight units)
+     * Returns the weight unit (or array of weight units) this plugin supports.
+     *
      * @see waShipping::allowedWeightUnit()
+     * @return array|string
      */
     public function allowedWeightUnit()
     {
-        return 'kg'; // return array('kg','lbs');
-
+        // return array('kg','lbs') or simple string: 'kg'
+        return 'kg';
     }
 
     /**
-     * Returns the general tracking information (HTML)
+     * Returns general tracking information (HTML).
+     *
      * @see waShipping::tracking()
-     * @example return 'Online shipment tracking: <a href="link">link</a>';
+     * @example return _wp('Online shipment tracking: <a href="link">link</a>.');
+     * @param string $tracking_id Optional tracking id specified by user.
+     * @return string
      */
     public function tracking($tracking_id = null)
     {
+        // this shipping plugin does not provide shipping tracking information
         return '';
     }
 
     /**
-     * Returns the list of printable forms this module offers
-     * @example <pre> return array(
-     *    'flatrate_form' => array(
-     *        'name' => _wp('Sample consignment note'),
-     *        'description' => _wp('Sample consignment description'),
+     * Returns array of printable forms this plugin offers
+     *
+     * @example return <pre>array(
+     *    'form_id' => array(
+     *        'name' => _wp('Printform name'),
+     *        'description' => _wp('Printform description'),
      *    ),
      * );</pre>
+     * @param waOrder $order Object containing order data
+     * @return array
      */
     public function getPrintForms(waOrder $order = null)
     {
+        // this shipping plugin does not generate printable forms
         return array();
     }
 
     /**
-     * Displays the print form content (HTML).
-     * Form id list is defined in getPrintForms() method
+     * Returns HTML code of specified printable form.
+     *
+     * @param string $id Printform id as defined in method getPrintForms()
+     * @param waOrder $order Order data object
+     * @param array $params Optional parameters to be passed to printform generation template
+     * @throws waException
+     * @return string Printform HTML
      */
     public function displayPrintForm($id, waOrder $order, $params = array())
     {
-        if ($id = 'flatrate_form') {
+        if ('flatrate_form' == $id) {
             $view = wa()->getView();
             $view->assign('order', $order);
             $view->assign('params', $params);
@@ -121,17 +151,46 @@ class flatrateShipping extends waShipping
     }
 
     /**
-     * Sets mask for destination addresses that this shipping module allows shipping to
+     * Limits the range of customer addresses to allow shipping to.
+     *
+     * @example <pre>return array(
+     *   'country' => 'usa', # or array('usa', 'can')
+     *   'region'  => 'NY',
+     *   # or array('NY', 'PA', 'CT');
+     *   # or omit 'region' item if you do not want to limit shipping to certain regions
+     * );</pre>
+     * @return array Return array() to allow shipping anywhere
      */
     public function allowedAddress()
     {
-        return array(
-
-        );
+        //this plugin allows shipping orders to any addresses without limitations
+        return array();
     }
 
+    /**
+     * Returns array of shipping address fields which must be requested during checkout.
+     *
+     * @see waShipping::requestedAddressFields()
+     * @example <pre>return array(
+     *     #requested field
+     *     'zip'     => array(),
+     *
+     *     #hidden field with pre-defined value;
+     *     'country' => array('hidden' => true, 'value' => 'rus', 'cost' => true),
+     *
+     *     #'cost' parameter means that field is used for calculation of approximate shipping cost during checkout
+     *     'region'  => array('cost' => true),
+     *     'city'    => array(),
+     *
+     *     #field is not requested
+     *     'street'  => false,
+     * );</pre>
+     * @return array|bool Return false if plugin does not request any fields during checkout;
+     * return array() if all address fields must be requested
+     */
     public function requestedAddressFields()
     {
+        //request either all or no address fields depending on the value of the corresponding plugin settings option
         return $this->prompt_address ? array() : false;
     }
 }
