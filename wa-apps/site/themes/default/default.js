a86f4bb90105c9468189b0fc9e019946b041dc43
Alexander Muzychenko <alexmuz@gmail.com>
2014-09-02 18:36:57 +0400

    site: theme Default 2.1

--- wa-apps/site/themes/default/default.js
+++ wa-apps/site/themes/default/default.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    // custom LOGO position adjustment
+    if ($('#logo').length)
+    {
+        $(window).load(function(){
+        
+            var _logo_height = $('#logo').height();
+            var _logo_vertical_shift = Math.round((_logo_height-25)/2);
+            
+            $('#header-container').css('padding-top', _logo_vertical_shift+'px');
+            $('#logo').css('margin-top', '-'+_logo_vertical_shift+'px');
+        
+        });
+    }
+
+    // SIDEBAR slide menu
     if ($('.slidemenu').length)
     {
         var _back_lbl = 'Back';
@@ -15,45 +30,26 @@ $(document).ready(function() {
             setTitle            : true,
             scrollToTopSpeed    : 200,
             backOnTop           : false,
-            beforeLoad          : function(){}
+            beforeLoad          : function(){ $('h1.category-name').append(' <i class="icon24 loading"></i>'); }
         });
     }
-    
-    // MOBILE slide-in menu
-    $('.apps-toggle').click( function(){
-        if (!$('.apps-negative').length) {
-            $('body').prepend($('header .apps').clone().removeClass('apps').addClass('apps-negative'));
-            $('.apps-negative').hide().slideToggle(200);
+
+    // MOBILE nav slide-out menu
+    $('#mobile-nav-toggle').click( function(){
+        if (!$('.nav-negative').length) {
+            $('body').prepend($('header .apps').clone().removeClass('apps').addClass('nav-negative'));
+            $('body').prepend($('header .auth').clone().addClass('nav-negative'));
+            $('body').prepend($('header .search').clone().addClass('nav-negative'));
+            $('body').prepend($('header .offline').clone().addClass('nav-negative'));
+            $('.nav-negative').hide().slideToggle(200);
         } else {
-            $('.apps-negative').slideToggle(200);
+            $('.nav-negative').slideToggle(200);
         }
-    });
-    
-    // SIDEBAR HEADER click
-    // on devices without :hover event (handheld tablets such as iPad) clicking on sidebar header link should show the sidebar
-    if ( (!!('ontouchstart' in window)) && window.matchMedia("only screen and (min-width: 760px) and (max-width: 1024px)").matches )
-    {
-        $('.nav-sidebar-body').css('opacity',1);
-        $('.nav-sidebar-body').hide();
-        $('a.nav-sidebar-header').click(function(){        
-            {
-                $('.nav-sidebar-body').slideToggle(200);
-                return false;
-            }
-        });
-    }
-        
-    // FOLLOW US toggle
-    $('a#followus-leash-toggler').click(function(){
-
-        $('#followus-leash-inner').slideToggle(200);
-        $('a#followus-leash-toggler i.icon16.toggle-arrow').toggleClass('uarr');
-        $('a#followus-leash-toggler i.icon16.toggle-arrow').toggleClass('darr');
+        $("html, body").animate({ scrollTop: 0 }, 200);
         return false;
-
     });
-    
-    // FOLLOW US Mailer subscribe
+        
+    // MAILER app email subscribe form
     $('#mailer-subscribe-form input.charset').val(document.charset || document.characterSet);
     $('#mailer-subscribe-form').submit(function() {
         var form = $(this);
@@ -72,48 +68,38 @@ $(document).ready(function() {
 
         $('#mailer-subscribe-iframe').load(function() {
             $('#mailer-subscribe-form').hide();
+            $('#mailer-subscribe-iframe').hide();
             $('#mailer-subscribe-thankyou').show();
         });
     });
     
-    // LOGO position adjustment
-    if ($('#logo').length)
+    // STICKY CART for non-mobile
+    if (!window.matchMedia("only screen and (max-width: 760px)").matches)
     {
-        $(window).load(function(){
-        
-            var _logo_height = $('#logo').height();
-            var _logo_vertical_shift = Math.round((_logo_height-25)/2);
-            
-            $('#header-container').css('padding-top', _logo_vertical_shift+'px');
-            $('#logo').css('margin-top', '-'+_logo_vertical_shift+'px');
-        
+        $(window).scroll(function(){
+               
+           	if ( $(this).scrollTop() >= 110 && !$("#cart").hasClass( "fixed" ) && !$("#cart").hasClass( "empty" ) && !($(".cart-summary-page")).length )
+           	{
+           	    $("#cart").hide();
+           	    
+           	    $("#cart").addClass( "fixed" );       	    
+           	    if ($('#cart-flyer').length)
+           	    {
+           	        var _width = $('#cart-flyer').width()+50;
+           	        var _offset_right = $(window).width() - $('#cart-flyer').offset().left - _width;
+           	        
+           	        $("#cart").css({ "right": _offset_right+"px", "width": _width+"px" });
+           	    }
+           	    
+           	    $("#cart").slideToggle(200);
+           	}
+           	else if ( $(this).scrollTop() < 100 && $("#cart").hasClass( "fixed" ) )
+        	{
+    	        $("#cart").removeClass( "fixed" );
+        	    $("#cart").css({ "width": "auto" });	   
+        	}
+           	
         });
     }
-
-    // STICKY CART
-    $(window).scroll(function(){
-           
-       	if ( $(this).scrollTop() >= 110 && !$("#cart").hasClass( "fixed" ) && !$("#cart").hasClass( "empty" ) && !($(".cart-summary-page")).length )
-       	{
-       	    $("#cart").hide();
-       	    
-       	    $("#cart").addClass( "fixed" );       	    
-       	    if ($('#cart-flyer').length)
-       	    {
-       	        var _width = $('#cart-flyer').width()+50;
-       	        var _offset_right = $(window).width() - $('#cart-flyer').offset().left - _width;
-       	        
-       	        $("#cart").css({ "right": _offset_right+"px", "width": _width+"px" });
-       	    }
-       	    
-       	    $("#cart").slideToggle(200);
-       	}
-       	else if ( $(this).scrollTop() < 100 && $("#cart").hasClass( "fixed" ) )
-    	{
-	        $("#cart").removeClass( "fixed" );
-    	    $("#cart").css({ "width": "auto" });	   
-    	}
-       	
-    });
 
 });
