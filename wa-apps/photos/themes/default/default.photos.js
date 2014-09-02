bf83883e7cc3e4ae5299c0f71a515c0eb05a24b9
Alexander Muzychenko <alexmuz@gmail.com>
2014-09-02 18:48:57 +0400

    photos: theme Default

--- wa-apps/photos/themes/default/default.photos.js
+++ wa-apps/photos/themes/default/default.photos.js
@@ -13,7 +13,7 @@ $(function() {
             itemWidth: '250',
             fillEmptySpace: false,
             flexibleWidth: true,
-            offset: 22,
+            offset: 0,
             outerOffset: 0,
             possibleFilters: [],
             resizeDelay: 50,
@@ -39,9 +39,107 @@ $(function() {
         list.waitForImages(applyLayout);
     }
     
-    $('.dropdown-sidebar .waSlideMenu-menu a').click(function(){
+    $('.waSlideMenu-menu a').click(function(){
+
         if ( !$(this).parent().hasClass('collapsible') && !$(this).parent().hasClass('waSlideMenu-back') )
-            $('#page-content').addClass('page-content'); // hack for single photo -> album navigation
+        {
+            // that was an end node click in waSlideMenu, so do forst redirect here (Photos app Default theme specific)
+            window.location.href = $(this).attr('href');
+        }
     });
+    
+    $('.slidemenu').on('afterLoadDone.waSlideMenu', function () {
+        $('img').retina();
+    });
+
+    if ($.fn.lazyLoad) {
+        var paging = $('.lazyloading-paging');
+        if (!paging.length) {
+            return;
+        }
+        // check need to initialize lazy-loading
+        var current = paging.find('li.selected');
+        if (current.children('a').text() != '1') {
+            return;
+        }
+        paging.hide();
+        var win = $(window);
+
+        var times = parseInt(paging.data('times'), 10);
+        var link_text = paging.data('linkText') || 'Load more';
+
+        // prevent previous launched lazy-loading
+        win.lazyLoad('stop');
+
+        // check need to initialize lazy-loading
+        var next = current.next();
+        if (next.length) {
+            win.lazyLoad({
+                container: '#photo-list',
+                load: function() {
+                    win.lazyLoad('sleep');
+                    var paging = $('.lazyloading-paging').hide();
+
+                    // determine actual current and next item for getting actual url
+                    var current = paging.find('li.selected');
+                    var next = current.next();
+                    var url = next.find('a').attr('href');
+                    if (!url) {
+                        win.lazyLoad('stop');
+                        return;
+                    }
+
+                    var photo_list = $('#photo-list');
+                    var loading = paging.parent().find('.loading').parent();
+                    if (!loading.length) {
+                        loading = $('<div><i class="icon16 loading"></i>Loading...</div>').insertBefore(paging);
+                    }
+
+                    loading.show();
+                    $.get(url, function(html) {
+                        var tmp = $('<div></div>').html(html);
+                        if ($.Retina) {
+                            tmp.find('#photo-list img').retina();
+                        }
+                        photo_list.append(tmp.find('#photo-list').children());
+                        var tmp_paging = tmp.find('.lazyloading-paging').hide();
+                        paging.replaceWith(tmp_paging);
+                        paging = tmp_paging;
+
+                        times -= 1;
+
+                        // check need to stop lazy-loading
+                        var current = paging.find('li.selected');
+                        var next = current.next();
+                        if (next.length) {
+                            if (!isNaN(times) && times <= 0) {
+                                win.lazyLoad('sleep');
+                                if (!$('.lazyloading-load-more').length) {
+                                    $('<a href="#" class="lazyloading-load-more">' + link_text + '</a>').insertAfter(paging)
+                                        .click(function() {
+                                            loading.show();
+                                            times = 1;      // one more time
+                                            win.lazyLoad('wake');
+                                            win.lazyLoad('force');
+                                            return false;
+                                        });
+                                }
+                            } else {
+                                win.lazyLoad('wake');
+                            }
+                        } else {
+                            win.lazyLoad('stop');
+                        }
+
+                        loading.hide();
+                        tmp.remove();
+
+                        photo_list.trigger('append_photo_list');
+
+                    });
+                }
+            });
+        }
+    }
     
 });
