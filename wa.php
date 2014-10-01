61d3663e0a71c3d12f18ba31a9e205b23ef8174d
Alexander Muzychenko <alexmuz@gmail.com>
2014-10-01 11:35:37 +0400

    fix wa.php

--- wa.php
+++ wa.php
@@ -8,6 +8,6 @@ if (PHP_SAPI !== 'cli') {
 require_once dirname(__FILE__).'/wa-config/SystemConfig.class.php';
 $wa = waSystem::getInstance(null, new SystemConfig('cli'));
 // Replace script name
-$_SERVER['argv'][0] = 'webasyst';
+array_splice($_SERVER['argv'], 1, 0, 'webasyst');
 // Run CLI
 $wa->dispatchCli($_SERVER['argv']);
