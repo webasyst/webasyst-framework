8c87622ee387f67f709dace27e6041d9712e9728
djvolk <dj_volk_@mail.ru>
2015-02-27 11:15:12 +0400

    Update html templates

--- import/1c.php
+++ import/1c.php
@@ -21,9 +21,9 @@ if (isset($_POST['products']))
 //    {	
 //	   $category = $category.' "'.$row['categoryID'].'",';
 //	}
-
+//
     //$query = mysql_query("SELECT * FROM SC_products", $db1);
-    $query = mysql_query("SELECT * FROM SC_products WHERE categoryID IN ('726', '675', '667', '677', '676', '666', '727', '672', '670', '674', '673', '671')", $db1);
+    $query = mysql_query("SELECT * FROM SC_products WHERE categoryID IN ('593', '634', '594', '633', '601', '628', '595', '661')", $db1);
     while ($row = mysql_fetch_array($query))
     {
 
@@ -70,22 +70,44 @@ if (isset($_POST['products']))
 							no_dublicate,
 							new,
 							brend,
-							related) VALUES ('".$row['productID']."',
-                                                                         '".$row['categoryID'].",".$cat."',
-                                                                         '".$row['Price']."',
-                                                                         '".$row['in_stock']."',
-                                                                         '".$default_picture['filename']."',
-                                                                         '".$row['slug']."',
-                                                                         '".$row['name_ru']."',
-                                                                         '".$row['brief_description_ru']."',
-                                                                         '".$row['description_ru']."',
-                                                                         '".$row['meta_title_ru']."',
-                                                                         '".$row['meta_description_ru']."',
-                                                                         '".$row['meta_keywords_ru']."',
-                                                                         '".$row['no_dublicate']."',
-                                                                         '".$row['new']."',
-                                                                         '".$brend['name']."',
-                                                                         '".$related."')", $db1);
+							related) VALUES ('".mysql_real_escape_string($row['productID'])."',
+                                                                         '".mysql_real_escape_string($row['categoryID']).",".mysql_real_escape_string($cat)."',
+                                                                         '".mysql_real_escape_string($row['Price'])."',
+                                                                         '".mysql_real_escape_string($row['in_stock'])."',
+                                                                         '".mysql_real_escape_string($default_picture['filename'])."',
+                                                                         '".mysql_real_escape_string($row['slug'])."',
+                                                                         '".mysql_real_escape_string($row['name_ru'])."',
+                                                                         '".mysql_real_escape_string($row['brief_description_ru'])."',
+                                                                         '".mysql_real_escape_string($row['description_ru'])."',
+                                                                         '".mysql_real_escape_string($row['meta_title_ru'])."',
+                                                                         '".mysql_real_escape_string($row['meta_description_ru'])."',
+                                                                         '".mysql_real_escape_string($row['meta_keywords_ru'])."',
+                                                                         '".mysql_real_escape_string($row['no_dublicate'])."',
+                                                                         '".mysql_real_escape_string($row['new'])."',
+                                                                         '".mysql_real_escape_string($brend['name'])."',
+                                                                         '".mysql_real_escape_string($related)."')", $db1);
+//        echo mysql_errno() . ": " . mysql_error() . "\n";
+//        if (mysql_errno() != 0)
+//        {
+//            echo '<br>';
+//        echo $row['productID'].'<br>';
+//        echo $row['categoryID'].",".$cat.'<br>';
+//        echo $row['Price'].'<br>';
+//        echo $row['in_stock'].'<br>';
+//        echo $default_picture['filename'].'<br>';
+//        echo $row['slug'].'<br>';
+//        echo $row['name_ru'].'<br>';
+//        echo '8'.$row['brief_description_ru'].'<br>';
+//        echo '9'.$row['description_ru'].'<br>';
+//        echo '10'.$row['meta_title_ru'].'<br>';
+//        echo '11'.$row['meta_description_ru'].'<br>';
+//        echo '12'.$row['meta_keywords_ru'].'<br>';
+//        echo '13'.$row['no_dublicate'].'<br>';
+//        echo '14'.$row['new'].'<br>';
+//        echo '15'.$brend['name'].'<br>';
+//        echo '16'.$related.'<br>';
+//        echo '<br><br><br>';
+//        }  
         if ($insert == true) $products_true++;
         else $products_false++;
     }
@@ -110,12 +132,12 @@ if (isset($_POST['options']))
             $variant = mysql_fetch_array($query_variant);
 
             $insert = mysql_query("INSERT INTO 1C_options (`id`,
-                                                       `option`,
-													   `value`,
-													   `in_stock`) VALUES ('".$row['id']."',
-													                     'razmer',
-													                     '".$variant['option_value_ru']."',
-													                     '".$row_options['in_stock']."')", $db1);
+                                                           `option`,
+							   `value`,
+							    `in_stock`) VALUES ('".$row['id']."',
+									        'razmer',
+									        '".$variant['option_value_ru']."',
+										'".$row_options['in_stock']."')", $db1);
         }
         if ($insert == true) $options_true++;
         else $options_false++;
@@ -130,29 +152,29 @@ if (isset($_POST['options_clear']))
 if (isset($_POST['categories']))
 {
     //$query = mysql_query("SELECT * FROM SC_categories WHERE categoryID != 1", $db1);
-	$query = mysql_query("SELECT * FROM SC_categories WHERE categoryID = 665 OR categoryID = 669 OR parent = 665 OR parent = 669", $db1);
+	$query = mysql_query("SELECT * FROM SC_categories WHERE categoryID = 593 OR parent = 593", $db1);
 	
     while ($row = mysql_fetch_array($query))
     {
         $insert = mysql_query("INSERT INTO 1C_categories (id,
                                                           parent,
-													      slug,
+							  slug,
                                                           name_ru,
                                                           description_ru,
-													      description_second_ru, 
-													      meta_title_ru,
-													      meta_description_ru,
-													      meta_keywords_ru,
-													      no_dublicate) VALUES ('".$row['categoryID']."',
-														                         '".$row['parent']."',
-																				 '".$row['slug']."',
-																				 '".$row['name_ru']."',
-																				 '".$row['description_ru']."',
-																				 '".$row['description_second_ru']."',
-																				 '".$row['meta_title_ru']."',
-																				 '".$row['meta_description_ru']."',
-																				 '".$row['meta_keywords_ru']."',
-																				 '".$row['no_dublicate']."')", $db1);
+                                                          description_second_ru, 
+                                                          meta_title_ru,
+                                                          meta_description_ru,
+                                                          meta_keywords_ru,
+                                                          no_dublicate) VALUES ('".$row['categoryID']."',
+                                                                                '".$row['parent']."',
+                                                                                '".mysql_real_escape_string($row['slug'])."',
+                                                                                '".mysql_real_escape_string($row['name_ru'])."',
+                                                                                '".mysql_real_escape_string($row['description_ru'])."',
+                                                                                '".mysql_real_escape_string($row['description_second_ru'])."',
+                                                                                '".mysql_real_escape_string($row['meta_title_ru'])."',
+                                                                                '".mysql_real_escape_string($row['meta_description_ru'])."',
+                                                                                '".mysql_real_escape_string($row['meta_keywords_ru'])."',
+                                                                                '".mysql_real_escape_string($row['no_dublicate'])."')", $db1);
         if ($insert == true) $categories_true++;
         else $categories_false++;
     }
@@ -165,13 +187,13 @@ if (isset($_POST['categories_clear']))
 
 if (isset($_POST['pictures']))
 {
-    //$query = mysql_query("SELECT * FROM SC_product_pictures", $db1);
-    $query = mysql_query("SELECT * FROM SC_product_pictures LEFT JOIN SC_products ON SC_product_pictures.productID=SC_products.productID  WHERE SC_products.categoryID IN ('726', '675', '667', '677', '676', '666', '727', '672', '670', '674', '673', '671')", $db1);
+    $query = mysql_query("SELECT * FROM SC_product_pictures", $db1);
+    //$query = mysql_query("SELECT * FROM SC_product_pictures LEFT JOIN SC_products ON SC_product_pictures.productID=SC_products.productID  WHERE SC_products.categoryID IN ('726', '675', '667', '677', '676', '666', '727', '672', '670', '674', '673', '671')", $db1);
     while ($row = mysql_fetch_array($query))
     {
         $insert = mysql_query("INSERT INTO 1C_pictures (productID,
                                                         filename,
-													    thumbnail,
+							thumbnail,
                                                         enlarged) VALUES ('".$row['productID']."',
 														                  '".$row['filename']."',
 																		  '".$row['thumbnail']."',
