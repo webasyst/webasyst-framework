a1401ffd6b0012c700a0098293ffe82db23125cd
Alexander Muzychenko <alexmuz@gmail.com>
2015-07-24 20:04:44 +0300

    payment plugin chasepaymentech

--- wa-plugins/payment/chasepaymentech/lib/config/requirements.php
+++ wa-plugins/payment/chasepaymentech/lib/config/requirements.php
@@ -1,11 +1,2 @@
 <?php
-return array(
-    'php.curl' => array(
-        'strict' => true,
-        'value'  => 1,
-    ),
-    'php.simplexml' => array(
-        'strict' => true,
-        'value' => 1
-    )
-);
+return array();
