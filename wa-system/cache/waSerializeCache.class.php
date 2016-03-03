webasyst/webasyst-framework

commit fe7ea699a1b9ff6f50fceb4fe202ecd2dff603c1

Update waSerializeCache::writeToFile
Не нужно создавать кеш при ttl = 0

wa-system/cache/waSerializeCache.class.php
@@ -18,8 +18,11 @@ class waSerializeCache extends waFileCache
     
     protected function writeToFile($file, $v)
     {
-        $data = serialize(array('time' => time(), 'ttl' => $this->ttl, 'value' => $v));
+        if ($this->ttl === 0) {
+            return null;
+        }
         if (!file_exists($file) || is_writable($file)) {
+            $data = serialize(array('time' => time(), 'ttl' => $this->ttl, 'value' => $v));
             $r = @file_put_contents($file, $data);
             if ($r) {
                 @chmod($file, 0664);

commit 142608bd945bc9551a558782ea7c435de29afbfb

Fix caches to work with TTL == 0 meaning infinite cache time; TTL < 0 means do not cache. This restores previous behaviour broken by commit fe7ea699a1b9ff6f50fceb4fe202ecd2dff603c1

wa-system/cache/waSerializeCache.class.php
@@ -13,12 +13,12 @@
  * @subpackage cache
  */
 
-class waSerializeCache extends waFileCache 
+class waSerializeCache extends waFileCache
 {
-    
+
     protected function writeToFile($file, $v)
     {
-        if ($this->ttl === 0) {
+        if ($this->ttl < 0) {
             return null;
         }
         if (!file_exists($file) || is_writable($file)) {
@@ -46,5 +46,5 @@ class waSerializeCache extends waFileCache
             }
         }
         return null;
-    }    
+    }
 }

wa-system/cache/waVarExportCache.class.php
@@ -17,7 +17,7 @@ class waVarExportCache extends waFileCache
 {
     protected function writeToFile($file, $v)
     {
-        if ($this->ttl === 0) {
+        if ($this->ttl < 0) {
             return true;
         }
         $result = waUtils::varExportToFile($v, $file);