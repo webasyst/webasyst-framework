84168364118c33b6c85e114baf9db8c7f1bfd6e7
Alexkurd <alexkurd@gmail.com>
2016-04-20 21:13:31 +0300

    Added files via upload

---
+++ wa-system/locale/data/no_NO.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+    'iso3' => 'nor',
+    'name' => 'Norsk',
+    'region' => 'Norge',
+    'english_name' => 'Norwegian',
+    'english_region' => 'Norway',
+    'date_formats' => array(
+        'humandate' => 'j. F, Y',
+        'date' => 'd.m.Y',
+        'datetime' => 'd.m.Y H:i',
+        'fulldatetime' => 'd.m.Y H:i:s'
+    ),
+    'decimal_point' => ',',
+    'frac_digits' => '2',
+    'thousands_sep' => '.',
+    'amount_in_words' => array(
+        'delim' => array(
+            10 => 'und',
+            100 => '',
+            1000 => ''
+        ),
+        'order' => array(
+            10 => false
+        ),
+        'plural' => array(
+            1000000 => 2
+        )
+    )
+);
---
+++ wa-system/locale/data/uk_UA.php
@@ -0,0 +1,34 @@
+<?php
+    return array(
+        'iso3' => 'ukr',
+        'name' => 'Українська',
+        'region' => 'Україна',
+        'english_name' => 'Ukraine',
+        'english_region' => 'Ukraine',
+        'date_formats' => array(
+            'humandate' => 'j f Y',
+            'date' => 'd.m.Y',
+            'dtime' => 'd.m H:i',
+            'datetime' => 'd.m.Y H:i',
+            'fulldatetime' => 'd.m.Y H:i:s'
+        ),
+        'date_month_postfix' => 'я',
+        'decimal_point' => ',',
+        'frac_digits' => '2',
+        'thousands_sep' => ' ',
+        'first_day' => 1,
+        'amount_in_words' => array(
+            'plural' => array(
+                1000 => 2
+            )
+        ),
+        'translit_table' => array(
+            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', '?' => 'G', 'Д' => 'D', 'Е' => 'E', 'Є' => 'Ye', 'Ж' => 'Zh', 'З' => 'Z', 'И' => 'Y', 'І' => 'I', 'Ї' => 'Yi', 'Й' => 'Y',
+            'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F',
+            'Х' => 'Kh', 'Ц' => 'Ts', 'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Shch', 'Ь' => '', 'Ю' => 'Yu', 'Я' => 'Ya'
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', '?' => 'g', 'д' => 'd', 'е' => 'e', 'є' => 'ye', 'ж' => 'zh', 'з' => 'z', 'и' => 'y', 'і' => 'і', 'ї' => 'yi', 'й' => 'y',
+            'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f',
+            'х' => 'kh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch', 'ь' => '', 'ю' => 'yu', 'я' => 'ya',
+        ),
+        'currency' => 'UAH',
+    );
