fa8e215d50fcd17d2f78b6a78dfbdef9e705b2c6
Leonix <leonix@motd.ru>
2016-09-26 16:05:48 +0300

    liqpay v.3.0.0: Переход на новую версию API 3.0 (необходимо перенастроить плагин).

--- wa-plugins/payment/liqpay/lib/config/settings.php
+++ wa-plugins/payment/liqpay/lib/config/settings.php
@@ -1,50 +1,21 @@
 <?php
 return array(
-    'merchant_id'    => array(
+    'public_key' => array(
         'value'        => '',
-        'title'        => 'ID продавца',
-        'description'  => 'Идентификатор продавца, выданный платежной системой',
+        'title'        => 'Публичный ключ',
+        'description'  => 'Идентификатор магазина. Получить ключ можно в настройках магазина вашего аккаунта в платежной системе.',
         'control_type' => waHtmlControl::INPUT,
     ),
-
-    'secret_key'     => array(
+    'secret_key' => array(
         'value'        => '',
-        'title'        => 'Подпись',
-        'description'  => 'Скопируйте значение «Signature for other operations» из вашего аккаунта в платежной системе',
+        'title'        => 'Приватный ключ',
+        'description'  => ' Получить ключ можно в настройках магазина вашего аккаунта в платежной системе.',
         'control_type' => waHtmlControl::PASSWORD,
     ),
-
-    'gateway'        => array(
-        'value'        => 'card, liqpay, delayed',
-        'title'        => 'Способ оплаты',
-        'description'  => '',
-        'control_type' => waHtmlControl::SELECT,
-        'options'      => array(
-            'card,liqpay,delayed' => 'на выбор покупателя',
-            'card'                => 'банковской картой',
-            'liqpay'              => 'из аккаунта LiqPay',
-            'delayed'             => 'наличными',
-        ),
-    ),
-
-    'order_prefix'   => array(
-        'value'        => '',
-        'title'        => 'Префикс номера счета',
-        'description'  => 'Допускается использовать цифры и латинские буквы',
-        'control_type' => waHtmlControl::INPUT,
-    ),
-
-    'bugfix'         => array(
+    'sandbox'    => array(
         'value'        => true,
-        'title'        => 'Добавлять случайное число к номеру счета',
-        'description'  => 'Позволяет обеспечить абсолютную уникальность Order ID платежной системы LiqPay при повторных оплатах счета',
+        'title'        => 'Тестовый режим',
+        'description'  => '',
         'control_type' => waHtmlControl::CHECKBOX,
-    ),
-
-    'customer_phone' => array(
-        'value'        => 'phone',
-        'title'        => 'Номер телефона покупателя',
-        'description'  => 'Выберите поле в форме регистрации покупателя, предназначенное для ввода номера телефона',
-        'control_type' => waHtmlControl::CONTACTFIELD,
     ),
 );
