aa2fea3e4f5938a4007fa47fcf0e449acac57dd1
Alexkurd <alexkurd@gmail.com>
2017-11-07 17:46:38 +0300

    Update it_IT.php

--- wa-system/locale/data/it_IT.php
+++ wa-system/locale/data/it_IT.php
@@ -1,25 +1,25 @@
 <?php
 
-return array (
-    'currency' => 'EUR',
-    'name' => 'Italiano',
-    'region' => 'Italia',
-    'english_name' => 'Italian',
-    'english_region' => 'Italy',
-    'thousands_sep' => '.',
-    'decimal_point' => ',',
-    'frac_digits' => '2',
-    'first_day' => '1',
-    'iso3' => 'ita',
-    'amount_in_words' => array(
-        'delim' => array(
-            10 => '',
-            100 => '',
-            1000 => ''
-        ),
-        'offset' => array(
-            1 => -1
-        ),
-        'single' => true
-    )
-);
+    return array (
+        'currency' => 'EUR',
+        'name' => 'Italiano',
+        'region' => 'Italia',
+        'english_name' => 'Italian',
+        'english_region' => 'Italy',
+        'thousands_sep' => '.',
+        'decimal_point' => ',',
+        'frac_digits' => '2',
+        'first_day' => '1',
+        'iso3' => 'ita',
+        'amount_in_words' => array(
+            'delim' => array(
+                10 => '',
+                100 => '',
+                1000 => ''
+            ),
+            'offset' => array(
+                1 => -1
+            ),
+            'single' => true
+        )
+    );
