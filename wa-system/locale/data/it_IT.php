42364be424d6d958922ca6f115fb477070520cdd
Alexkurd <alexkurd@gmail.com>
2017-11-07 17:41:33 +0300

    Update it_IT.php

--- wa-system/locale/data/it_IT.php
+++ wa-system/locale/data/it_IT.php
@@ -1,10 +1,16 @@
 <?php
 
-return array(
+return array (
+    'currency' => 'EUR',
     'name' => 'Italiano',
     'region' => 'Italia',
     'english_name' => 'Italian',
     'english_region' => 'Italy',
+    'thousands_sep' => '.',
+    'decimal_point' => ',',
+    'frac_digits' => '2',
+    'first_day' => '1',
+    'iso3' => 'ita',
     'amount_in_words' => array(
         'delim' => array(
             10 => '',
@@ -16,5 +22,4 @@ return array(
         ),
         'single' => true
     )
-
 );
