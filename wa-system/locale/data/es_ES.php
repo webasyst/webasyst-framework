a294e8182ab3da4f9e712142c8fae2fd21bf074a
Alexkurd <alexkurd@gmail.com>
2017-11-07 17:44:37 +0300

    Update es_ES.php

--- wa-system/locale/data/es_ES.php
+++ wa-system/locale/data/es_ES.php
@@ -1,14 +1,14 @@
 <?php
 
-return array (
-  'currency' => 'EUR',
-  'name' => 'Español',
-  'region' => 'España',
-  'english_name' => 'Spanish',
-  'english_region' => 'Spain',
-  'thousands_sep' => '.',
-  'decimal_point' => ',',
-  'frac_digits' => '2',
-  'first_day' => '1',
-  'iso3' => 'esp',
-);
+    return array (
+        'currency' => 'EUR',
+        'name' => 'Español',
+        'region' => 'España',
+        'english_name' => 'Spanish',
+        'english_region' => 'Spain',
+        'thousands_sep' => '.',
+        'decimal_point' => ',',
+        'frac_digits' => '2',
+        'first_day' => '1',
+        'iso3' => 'esp',
+    );
