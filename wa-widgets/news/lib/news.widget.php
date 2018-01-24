d96d535bf9639b879c8a93064815c47fd62e9ee2
Leonix <leonix@motd.ru>
2018-01-24 17:01:41 +0300

    wa-widgets/news v.1.3.0 No longer uses outdated Google Feed API.

--- wa-widgets/news/lib/news.widget.php
+++ wa-widgets/news/lib/news.widget.php
@@ -17,9 +17,19 @@ class newsWidget extends waWidget
         if ($rss_url == 'custom') {
             $rss_url = ifempty($settings['custom_rss_feed']);
         }
+
+        $data = $this->getRssData($rss_url);
+
         $this->display(array(
-            'rss_url' => $rss_url,
-            'uniqid' => 'n'.uniqid(true),
+            'rss_url'      => $rss_url,
+            'channel'      => array(
+                'name' => ifset($data['channel']['name']),
+                'link' => ifset($data['channel']['link']),
+            ),
+            'items'        => ifset($data['items'], array()),
+            'widget_id'    => $this->id,
+            'widget_url'   => $this->getStaticUrl(),
+            'uniqid'       => 'n'.uniqid(true),
         ), $this->getTemplatePath('Default.html'));
     }
 
@@ -27,31 +37,25 @@ class newsWidget extends waWidget
     protected function getSettingsConfig()
     {
         $feeds = array(
-            'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml'
-                => 'New York Times',
-            'http://feeds.washingtonpost.com/rss/world'
-                => 'Washington Post',
-            'http://www.theguardian.com/world/rss'
-                => 'The Guardian',
+            'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml' => 'New York Times',
+            'http://feeds.washingtonpost.com/rss/world'                         => 'Washington Post',
+            'http://www.theguardian.com/world/rss'                              => 'The Guardian',
         );
 
         if (wa()->getLocale() == 'ru_RU') {
             $feeds = array_merge(array(
-                'https://news.yandex.ru/index.rss'
-                    => 'Яндекс.Новости',
-                'http://russian.rt.com/rss/'
-                    => 'Russia Today (на русском)',
+                'https://news.yandex.ru/index.rss' => 'Яндекс.Новости',
+                'http://russian.rt.com/rss/'       => 'Russia Today (на русском)',
+                'https://meduza.io/rss/all'        => 'Meduza.io',
             ), $feeds);
         } else {
             $feeds = array_merge($feeds, array(
-                'http://rt.com/rss/news/'
-                    => 'Russia Today',
+                'http://rt.com/rss/news/' => 'Russia Today',
             ));
         }
 
         $feeds = array_merge($feeds, array(
-            'custom'
-                => 'RSS feed:',
+            'custom' => 'RSS feed:',
         ));
 
         $result = parent::getSettingsConfig();
@@ -71,10 +75,36 @@ class newsWidget extends waWidget
         self::$instance || wa('webasyst')->getWidget('news');
 
         return self::$instance->display(array(
-            'field_name' => $field_name,
+            'field_name'  => $field_name,
             'field_value' => $field_params['value'],
-            'uniqid' => 'n'.uniqid(true),
+            'uniqid'      => 'n'.uniqid(true),
         ), self::$instance->getTemplatePath('CustomRssControl.html'), true);
+    }
+
+    public function getRssData($rss_url)
+    {
+        if (!$rss_url) {
+            return array();
+        }
+
+        $xml = @simplexml_load_file($rss_url);
+        if ($xml) {
+            $data = array(
+                'channel' => array(
+                    'name' => $xml->channel->title,
+                    'link' => $xml->channel->link,
+                ),
+                'items'   => array(),
+            );
+            foreach ($xml->xpath('//item') as $item) {
+                $data['items'][] = array(
+                    'title' => $item->title,
+                    'link'  => $item->link,
+                    'date'  => $item->pubDate,
+                );
+            }
+            return $data;
+        }
     }
 }
 
