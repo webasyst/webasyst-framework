d0ac169170a9b2e099faaba540c8fc3a3f742992
Leonix <leonix@motd.ru>
2018-02-09 14:19:52 +0300

    wa-plugins/shipping/worldwide: 1.0.0

---
+++ wa-plugins/shipping/worldwide/lib/config/plugin.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'name'        => /*_wp*/('Worldwide'),
+    'description' => /*_wp*/('Reliable international delivery tailored to local conditions.'),
+    'icon'        => 'img/worldwide16.png',
+    'logo'        => 'img/worldwide.png',
+    'version'     => '1.0.0',
+    'vendor'      => 'webasyst',
+);
---
+++ wa-plugins/shipping/worldwide/lib/config/settings.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'weight_dimension' => array(
+        'value' => 'kg'
+    ),
+    'currency'         => array(
+        'value' => 'USD',
+    ),
+    'delivery_table'   => array(
+        'value' => array()
+    ),
+    'weights'          => array(
+        'value' => 'all'
+    ),
+);
