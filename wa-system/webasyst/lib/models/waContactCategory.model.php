26712d7c9c23d0b91d593c7dda5bf71f29a6c065
Serge Rodovnichenko <sergerod@gmail.com>
2018-04-22 00:44:40 +0300

    Refactored waContactModel
    - Refactored code to use model methods instead of raw SQL where it is possible
    - Added typehinting and typecasting
    - Docblocks reformatted

--- wa-system/webasyst/lib/models/waContactCategory.model.php
+++ wa-system/webasyst/lib/models/waContactCategory.model.php
@@ -4,29 +4,45 @@ class waContactCategoryModel extends waModel
 {
     protected $table = 'wa_contact_category';
 
-    /** Create category
+    /**
+     * Create category with given $name
+     *
      * @param string $name New category name
-     * @return int new id */
+     * @return int new id
+     */
     public function add($name)
     {
         return $this->insert(array('name' => $name));
     }
 
+    /**
+     * @param array|int $value
+     * @return array|null
+     */
     public function getById($value)
     {
-        $data = parent::getById($value);
-        if ($data && $data['system_id'] && wa()->appExists($data['system_id'])) {
-            $app = wa()->getAppInfo($data['system_id']);
-            $data['name'] = $app['name'];
-            $data['icon'] = wa()->getRootUrl(true).$app['icon'][16];
+        if (!$value) {
+            return null;
         }
-        return $data;
+
+        $data = parent::getById((array)$value);
+
+        foreach ($data as &$row) {
+            if ($row) {
+                $row = $this->getAppInfo((string)ifset($row, 'system_id', '')) + $row;
+            }
+        }
+        unset($row);
+
+        return count($value) > 1 ? $data : array_shift($data);
     }
 
     /**
      * Delete category by ID
+     *
      * @param int $id
      * @return boolean
+     * @deprecated use deleteById() instead. This will give no change to orphans in linked tables
      */
     public function delete($id)
     {
@@ -35,15 +51,36 @@ class waContactCategoryModel extends waModel
         return $this->deleteById($id);
     }
 
-    /** @return array id => name */
+    /**
+     * Delete category by ID
+     *
+     * @param int $id
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        $ccm = new waContactCategoriesModel();
+        $ccm->deleteByField('category_id', $id);
+
+        return parent::deleteById($id);
+    }
+
+    /**
+     * Returns all names of categories indexed by its ID
+     *
+     * @return array id => name
+     */
     public function getNames()
     {
-        $sql = "SELECT id, name FROM {$this->table} ORDER BY name";
-        return $this->query($sql)->fetchAll('id', true);
+        return $this->select('id, name')
+            ->order('name')
+            ->query()
+            ->fetchAll('id', true);
     }
 
     /**
      * Update members count
+     *
      * @param int $id
      * @param int $count
      */
@@ -54,17 +91,18 @@ class waContactCategoryModel extends waModel
 
     /**
      * Recalculate counters of categories
+     *
      * @param int|array|null $id
      */
     public function recalcCounters($id = null)
     {
         $where = '';
         if ($id) {
-            $ids = array_map('intval', (array) $id);
+            $ids = array_filter(array_map('intval', (array)$id));
             if (!$ids) {
                 return;
             }
-            $where = "WHERE cc.id IN (".implode(',', $ids).")";
+            $where = "WHERE cc.id IN (" . implode(',', $ids) . ")";
         }
         $sql = "UPDATE `wa_contact_category` cc JOIN (
                 SELECT cc.id, COUNT(*) AS count FROM `wa_contact_category` cc
@@ -72,6 +110,7 @@ class waContactCategoryModel extends waModel
                 {$where}
                 GROUP BY cc.id) t ON t.id = cc.id
             SET cc.cnt = t.count";
+
         $this->exec($sql);
     }
 
@@ -82,16 +121,10 @@ class waContactCategoryModel extends waModel
      */
     public function getAll($key = null, $normalize = false)
     {
-        $sql = "SELECT * FROM {$this->table} ORDER BY name";
-        $data = $this->query($sql)->fetchAll($key, $normalize);
+        $data = $this->select('*')->order('name')->query()->fetchAll($key, $normalize);
+
         foreach ($data as &$row) {
-            if ($row['system_id']) {
-                if (wa()->appExists($row['system_id'])) {
-                    $app = wa()->getAppInfo($row['system_id']);
-                    $row['name'] = $app['name'];
-                    $row['icon'] = wa()->getRootUrl(true).$app['icon'][16];
-                }
-            }
+            $row = $this->getAppInfo(ifset($row, 'system_id', '')) + $row;
         }
         unset($row);
         return $data;
@@ -100,20 +133,38 @@ class waContactCategoryModel extends waModel
     /**
      * Category row with given system id.
      * Category is created with given name (matches $system_id if omitted) when it does not exist.
+     *
      * @param string $system_id
      * @param string $name
      * @return array
+     * @throws waException
      */
-    public function getBySystemId($system_id, $name=null)
+    public function getBySystemId($system_id, $name = null)
     {
         $cat = $this->getByField('system_id', $system_id);
         if (!$cat) {
             return $this->getById($this->insert(array(
                 'system_id' => $system_id,
-                'name' => $name ? $name : $system_id,
+                'name'      => $name ? $name : $system_id,
             )));
         }
         return $cat;
+    }
+
+    /**
+     * Get application properties by application ID
+     *
+     * @param string $system_id
+     * @return array
+     */
+    protected function getAppInfo($system_id)
+    {
+        if (empty($system_id) || !wa()->appExists($system_id)) {
+            return array();
+        }
+        $app = wa()->getAppInfo($system_id);
+
+        return array('name' => $app['name'], 'icon' => wa()->getRootUrl(true) . $app['icon'][16]);
     }
 }
 
