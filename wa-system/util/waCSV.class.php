f093625201921b4bb4aaac71cf6ded7de00e2ab5
Anton <WinterSilence@users.noreply.github.com>
2018-08-02 06:09:46 +0300

    Fix bugs and optimize waCSV
    Удалено неиспользуемое свойство format_id + нашлось применение для неиспользуемого свойства quote.
    Исправлена локализация сообщений исключений.
    Оптимизирован подсчет кол-ва строк (работает в 3-4 раза быстрее + использует меньше оперативки) + исправлен старый вариант - если строка пустая fgetcsv возвращает array(null).

--- wa-system/util/waCSV.class.php
+++ wa-system/util/waCSV.class.php
@@ -14,14 +14,12 @@ class waCSV
     protected $quote = '"';
     protected $length = 4096; // Max length of the row in the csv file
     protected $extensions = array('txt', 'csv');
-
-    protected $delimiter;
+    protected $delimiter = ",";
     /**
      * @var array
      */
-    protected $fields;
+    protected $fields = array();
     protected $first_line = false;
-    protected $format_id = false;
     public $encode = false;
 
     public static function getDelimiters()
@@ -60,13 +58,13 @@ class waCSV
 
     public function getPath($file)
     {
-        return waSystem::getInstance()->getTempPath()."/".$file;
+        return waSystem::getInstance()->getTempPath($file);
     }
 
     /**
      * Set Fields
      *
-     * @param $fields
+     * @param array $fields
      */
     public function setFields($fields)
     {
@@ -81,25 +79,26 @@ class waCSV
     /**
      * Upload file and returns path to the file
      *
+     * @todo Use waRequest::file() and waRequestFile methods.
      * @param $name
      * @return string
      */
     public function upload($name)
     {
         if (!isset($_FILES[$name]) || $_FILES[$name]['error']) {
-            throw new waException(_("Error uploading file"));
+            throw new waException(_ws("Error uploading file"));
         }
 
         $file_info = explode(".", $_FILES[$name]['name']);
         if (!in_array(strtolower(end($file_info)), $this->extensions)) {
-            throw new waException(_("Unknown extension *.").end($file_info));
+            throw new waException(_ws("Unknown extension *.").end($file_info));
         }
 
         $file = uniqid("csv").".csv";
         if (move_uploaded_file($_FILES[$name]['tmp_name'], $this->getPath($file))) {
             $this->file = $this->getPath($file);
         } else {
-            throw new waException(_('Error moving file'));
+            throw new waException(_ws('Error moving file'));
         }
         return $file;
     }
@@ -116,7 +115,7 @@ class waCSV
         if (file_put_contents($this->getPath($file), $content)) {
             $this->file = $this->getPath($file);
         } else {
-            throw new waException(_('Error moving file'));
+            throw new waException(_ws('Error moving file'));
         }
         return $file;
     }
@@ -134,11 +133,11 @@ class waCSV
     public function getInfo()
     {
         if (!$this->file || !file_exists($this->file)) {
-            throw new waException(_('File does not exist'));
+            throw new waException(_ws('File does not exist'));
         }
         $h = fopen($this->file, "r");
         if (!$h) {
-            throw new waException(_("Error open file"));
+            throw new waException(_ws("Error open file"));
         }
         // Read the first string
         $string = fgets($h, $this->length);
@@ -146,7 +145,7 @@ class waCSV
         // Get delimiter
         if (!$this->delimiter) {
             $max_count_fields = 0;
-            foreach(self::$delimiters as $i => $delimiter) {
+            foreach (self::$delimiters as $i => $delimiter) {
                 $delimiter = $delimiter[0];
                 $count_fields = count(explode($delimiter, $string));
                 if ($count_fields > $max_count_fields) {
@@ -159,7 +158,7 @@ class waCSV
         // Read fields
         rewind($h);
         $records = array();
-        $records[] = $fields = $this->encodeArray(fgetcsv($h, $this->length, $this->delimiter));
+        $records[] = $fields = $this->encodeArray(fgetcsv($h, $this->length, $this->delimiter, $this->quote));
         $fields_count = count($fields);
 
         // Count lines in files
@@ -168,7 +167,7 @@ class waCSV
             $n = 1;
         }
 
-        while ($n <= 10 && $string = $this->encodeArray(fgetcsv($h, $this->length, $this->delimiter)))    {
+        while ($n <= 10 && $string = $this->encodeArray(fgetcsv($h, $this->length, $this->delimiter, $this->quote))) {
             if ($this->notEmptyArray($string)) {
                 $records[] = $string;
                 $count = count($string);
@@ -184,16 +183,29 @@ class waCSV
                 $fields[] = "";
             }
         }
-
-        while ($data = fgetcsv($h, $this->length, $this->delimiter)) {
-            // Count only not empty strings
-            if ($data) {
-                $n++;
+        
+        if (class_exists('SplFileObject')) {
+            fclose($h);
+            
+            // Hack for fast count strings.
+            $file = new SplFileObject($this->file, 'r');
+            $file->setFlags(
+                SplFileObject::READ_CSV | SplFileObject::READ_AHEAD 
+                | SplFileObject::SKIP_EMPTY | SplFileObject::DROP_NEW_LINE
+            );
+            $file->setCsvControl($this->delimiter, $this->quote);
+            $file->seek(PHP_INT_MAX);
+            $n = $file->key() + 1;
+        } else {
+            while ($data = fgetcsv($h, $this->length, $this->delimiter, $this->quote)) {
+                // Count only not empty strings.
+                // A blank line will be returned as an array comprising a single null field. 
+                if ($data && $data != array(null)) {
+                    $n++;
+                }
             }
+            fclose($h);
         }
-
-        // Close file
-        fclose($h);
 
         return array(
             'delimiter' => $this->delimiter,
@@ -208,24 +220,23 @@ class waCSV
     {
         if ($this->encode && is_array($a)) {
             foreach ($a as &$v) {
-                @$v = iconv($this->encode, "utf-8//IGNORE", $v);
+                $v = @ iconv($this->encode, "utf-8//IGNORE", $v);
             }
         }
         return $a;
     }
 
-    protected function notEmptyArray($a)
+    protected function notEmptyArray(array $a)
     {
         if (!$a) {
             return false;
         }
-        $t = false;
         foreach ($a as $v) {
             if (trim($v)) {
-                $t = true;
+                return true;
             }
         }
-        return $t;
+        return false;
     }
 
     public function getDelimiterIndex($delimiter)
@@ -240,7 +251,7 @@ class waCSV
     /**
      * Read CSV-file and returns data
      *
-     * @param $limit
+     * @param int $limit
      * @return array
      */
     public function import($limit = 50)
@@ -248,16 +259,16 @@ class waCSV
         if (!$this->handler) {
             $this->handler = fopen($this->file, "r");
             if (!$this->handler) {
-                throw new waException(_("Error open file"));
+                throw new waException(_ws("Error open file"));
             }
             if (!$this->first_line) {
-                $fields = $this->encodeArray(fgetcsv($this->handler, $this->length, $this->delimiter));
+                $fields = $this->encodeArray(fgetcsv($this->handler, $this->length, $this->delimiter, $this->quote));
             }
         }
         $data = array();
         $i = 0;
         while (!feof($this->handler) && $limit > $i++) {
-            $real_data = $this->encodeArray(fgetcsv($this->handler, $this->length, $this->delimiter));
+            $real_data = $this->encodeArray(fgetcsv($this->handler, $this->length, $this->delimiter, $this->quote));
             if (!$this->notEmptyArray($real_data)) {
                 $i--;
                 continue;
@@ -283,5 +294,4 @@ class waCSV
 
         return $data;
     }
-
 }
