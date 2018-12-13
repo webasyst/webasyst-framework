08a3e733b013f0a334340a80026207b182d6c82e
Leonix <leonix@motd.ru>
2018-12-13 18:02:47 +0300

    wa-plugins/payment/payonline 1.2.0   * Использование новой версии формата фискальных данных ФФД 1.05     для настройки и передачи признаков предмета расчета и способа расчета.

--- wa-plugins/payment/payonline/lib/config/settings.php
+++ wa-plugins/payment/payonline/lib/config/settings.php
@@ -64,27 +64,119 @@ return array(
             .'<br>'
             .'— налог составляет 0%, 10% либо 18% и <em>включен</em> в стоимость элементов заказа и стоимость доставки'.
 
-            '
+            <<<HTML
 <script type="text/javascript">
 (function () {
-    $(\':input[name$="\[receipt\]"]\').unbind(\'change\').bind(\'change\', function (event) {
+    $(':input[name$="\[receipt\]"]').unbind('change').bind('change', function (event) {
         var show = this.checked;
         var fast = !event.originalEvent;
-        $(this).parents(\'form\').find(\':input[name$="\[sno\]"]\').each(function () {
+        var name = [
+            'payment_subject_type_product',
+            'payment_subject_type_service',
+            'payment_subject_type_shipping',
+            'payment_method_type'
+        ];
+        var selector = [];
+        for (var i = 0; i < name.length; i++) {
+            selector.push(':input[name$="\[' + name[i] + '\]"]');
+        }
+        selector = selector.join(', ');
+        $(this).parents('form').find(selector).each(function () {
             if (show) {
-                $(this).parents(\'div.field\').show(400);
+                $(this).parents('div.field').show(400);
             } else {
                 if (fast) {
-                    $(this).parents(\'div.field\').hide();
+                    $(this).parents('div.field').hide();
                 } else {
-                    $(this).parents(\'div.field\').hide(400);
+                    $(this).parents('div.field').hide(400);
                 }
             }
         })
-    }).trigger(\'change\');
+    }).trigger('change');
 })();
 </script>
-',
+HTML
+        ,
         'control_type' => waHtmlControl::CHECKBOX,
+    ),
+
+    'payment_subject_type_product'  => array(
+        'value'        => '1',
+        'title'        => 'Признак предмета расчета для товаров в чеках',
+        'description'  => 'Категория ваших товаров в чеке — для передачи в налоговую инспекцию.',
+        'control_type' => waHtmlControl::SELECT,
+        'options'      => array(
+            '1'  => 'товар',
+            '2'  => 'подакцизный товар',
+            '3'  => 'работа',
+            '4'  => 'услуга',
+            '5'  => 'ставка азартной игры',
+            '6'  => 'выигрыш азартной игры',
+            '7'  => 'лотерейный билет',
+            '8'  => 'выигрыш лотереи',
+            '9'  => 'предоставление РИД',
+            '10' => 'платеж',
+            '11' => 'агентское вознаграждение',
+            '12' => 'составной предмет расчета',
+            '13' => 'иной предмет расчета',
+        ),
+    ),
+    'payment_subject_type_service'  => array(
+        'value'        => '4',
+        'title'        => 'Признак предмета расчета для услуг в чеках',
+        'description'  => 'Категория ваших услуг для товаров в чеке — для передачи в налоговую инспекцию.',
+        'control_type' => waHtmlControl::SELECT,
+        'options'      => array(
+            '1'  => 'товар',
+            '2'  => 'подакцизный товар',
+            '3'  => 'работа',
+            '4'  => 'услуга',
+            '5'  => 'ставка азартной игры',
+            '6'  => 'выигрыш азартной игры',
+            '7'  => 'лотерейный билет',
+            '8'  => 'выигрыш лотереи',
+            '9'  => 'предоставление РИД',
+            '10' => 'платеж',
+            '11' => 'агентское вознаграждение',
+            '12' => 'составной предмет расчета',
+            '13' => 'иной предмет расчета',
+        ),
+    ),
+    'payment_subject_type_shipping' => array(
+        'value'        => '4',
+        'title'        => 'Признак предмета расчета для доставки в чеках',
+        'description'  => 'Категория услуги по доставке заказа в чеке — для передачи в налоговую инспекцию.',
+        'control_type' => waHtmlControl::SELECT,
+        'options'      => array(
+            '1'  => 'товар',
+            '2'  => 'подакцизный товар',
+            '3'  => 'работа',
+            '4'  => 'услуга',
+            '5'  => 'ставка азартной игры',
+            '6'  => 'выигрыш азартной игры',
+            '7'  => 'лотерейный билет',
+            '8'  => 'выигрыш лотереи',
+            '9'  => 'предоставление РИД',
+            '10' => 'платеж',
+            '11' => 'агентское вознаграждение',
+            '12' => 'составной предмет расчета',
+            '13' => 'иной предмет расчета',
+        ),
+    ),
+
+    'payment_method_type' => array(
+        'value'        => '1',
+        'title'        => 'Признак способа расчета в чеках',
+        'description'  => 'Категория способа оплаты всех позиций в чеке — для передачи в налоговую инспекцию.',
+        'control_type' => waHtmlControl::SELECT,
+        'options'      => array(
+            '1' => 'предоплата 100%',
+            '2' => 'частичная предоплата',
+            '3' => 'аванс',
+            '4' => 'полный расчет',
+            '5' => 'частичный расчет и кредит',
+            '6' => 'передача в кредит',
+            '7' => 'оплата кредита',
+        ),
     ),
 );
