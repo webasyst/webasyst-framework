8351bfef700295534f8b0829c37499c400913dcc
Anton <antonfedonjuk@gmail.com>
2019-02-15 13:48:43 +0300

    Fix bugs in webasystCreateSystempluginCli

--- wa-system/webasyst/lib/cli/webasystCreateSystemplugin.cli.php
+++ wa-system/webasyst/lib/cli/webasystCreateSystemplugin.cli.php
@@ -103,7 +103,7 @@ REPORT;
         return str_replace($this->template_id, $this->plugin_id, $file);
     }
 
-    protected function createConfig()
+    protected function createConfig($params = array())
     {
         return array(
             'name'        => empty($params['name']) ? ucfirst($this->plugin_id) : $params['name'],
@@ -116,13 +116,13 @@ REPORT;
 
     protected function create($params = array())
     {
-        $config = $this->createConfig();
+        $config = $this->createConfig($params);
 
         $structure = array(
             'lib/classes',
             'lib/vendors',
             'lib/config/plugin.php' => $config,
-            //TODO add plugin's images
+            'img'
         );
 
         $files = array(
@@ -152,7 +152,7 @@ REPORT;
         }
 
         $this->createStructure($structure);
-        $this->protect(array('lib', 'lib/config'));
+        $this->protect(array('lib', 'templates');
 
         return $config;
     }
