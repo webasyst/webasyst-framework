f253da01360f0e24a4dd0148d70c37d011b3adf1
Leonix <leonix@motd.ru>
2019-02-26 19:09:09 +0300

    site themes: Option to show brief working schedule of the online store instead of detailed schedule from store settings.

--- wa-apps/site/themes/nifty/nifty.js
+++ wa-apps/site/themes/nifty/nifty.js
@@ -1,5 +1,30 @@
-$(document).ready(function () {
+( function($) {
 
-    //add Site app's JS here
+    $(document).ready( function() {
+        init();
+    });
 
-});
+    function init() {
+        var $nav_list = $("#niftybox .app-navigation > .menu-h.dropdown");
+        if (!$nav_list.length) { return false; }
+
+        var is_mobile = false;
+
+        $nav_list.on("touchstart", ".collapsible > a", function() {
+            is_mobile = true;
+        });
+
+        $nav_list.on("touchend", ".collapsible > a", function() {
+            setTimeout( function() {
+                is_mobile = false;
+            }, 100);
+        });
+
+        $nav_list.on("click", ".collapsible > a", function() {
+            if (is_mobile) {
+                event.preventDefault();
+            }
+        });
+    }
+
+})(jQuery);
