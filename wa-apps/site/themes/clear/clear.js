107c5af2b3d60f929d3caefcf299b85b5bbe4bff
Leonix <leonix@motd.ru>
2019-06-26 17:50:47 +0300

    site theme clear:   * Fixed behavior of “All products” link in the mobile version.   * Fixed localization of “Add to cart” button in product sets     embedded in various apps’ design templates.

--- wa-apps/site/themes/clear/clear.js
+++ wa-apps/site/themes/clear/clear.js
@@ -1,5 +1,38 @@
-$(document).ready(function () {
+( function($) {
+    var is_touch_enabled = ("ontouchstart" in window);
 
-    //add Site app's JS here
+    $(document).ready( function () {
 
-});
+        if (is_touch_enabled) {
+            $("html").addClass("is-touch-enabled");
+
+            $(".app-navigation").on("click", ".menu-h.dropdown .collapsible", function(event) {
+                var has_menu = $(event.target).parent().hasClass("collapsible");
+                if (has_menu) {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    var $li = $(this);
+
+                    var active_class = "is-opened";
+
+                    // clear all
+                    $li.closest(".menu-h.dropdown")
+                        .find(".collapsible").removeClass(active_class);
+
+                    // mark parents
+                    $li.parents(".collapsible").addClass(active_class);
+
+                    // mark children
+                    if ($li.hasClass(active_class)) {
+                        $li.removeClass(active_class);
+                    } else {
+                        $li.addClass(active_class);
+                    }
+                }
+            });
+        }
+
+    });
+
+})(jQuery);
