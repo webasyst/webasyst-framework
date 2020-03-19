fca1667adac8abb143d0ebfd84bf2b5d16c414b4
Leonix <leonix@motd.ru>
2020-03-19 15:50:18 +0300

    Webasyst Framework v.1.13.7

--- wa-system/webasyst/lib/config/app.php
+++ wa-system/webasyst/lib/config/app.php
@@ -3,7 +3,7 @@
 return array(
     'name'         => 'Webasyst',
     'prefix'       => 'webasyst',
-    'version'      => '1.13.7', // developer preview
+    'version'      => '1.13.7',
     'critical'     => '1.13.7',
     'vendor'       => 'webasyst',
     'csrf'         => true,
