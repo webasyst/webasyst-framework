6a0ff2ec604b29f4f0ea6ba8a31ebd38f5eaeab0
woody-ru <woody@wood.ru>
2020-04-21 17:55:03 +0300

    Fixed 'payment_method_data' param of initial post request

--- wa-plugins/payment/yandexkassa/lib/yandexkassaPayment.class.php
+++ wa-plugins/payment/yandexkassa/lib/yandexkassaPayment.class.php
@@ -284,7 +284,7 @@ class yandexkassaPayment extends waPayment implements waIPayment, waIPaymentCanc
         );
 
         if (!empty($type)) {
-            $data['payment_method'] = array(
+            $data['payment_method_data'] = array(
                 'type' => $type,
             );
         }
