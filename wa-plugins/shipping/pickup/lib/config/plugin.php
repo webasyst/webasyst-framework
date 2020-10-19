639536f727520b6a7125816980c39965b25d67e1
Leonix <leonix@motd.ru>
2020-10-19 11:20:42 +0300

    wa-plugins/shipping/pickup v.1.0.1   * Fixed minor error on PHP version 7.4.

--- wa-plugins/shipping/pickup/lib/config/plugin.php
+++ wa-plugins/shipping/pickup/lib/config/plugin.php
@@ -4,6 +4,6 @@ return array(
     'description' => /*_wp*/('Self pickup from a nearby store location (customer selects location from the list).'),
     'icon'        => 'img/pickup16.png', //path to module 16x16 icon
     'logo'        => 'img/pickup.png', //path to module logo (recommended logo size: 60x32)
-    'version'     => '1.0.0',
+    'version'     => '1.0.1',
     'vendor'      => 'webasyst',
 );
