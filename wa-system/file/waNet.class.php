73c10d9bd0d27a5ff5473bef4ae04ecddb6d53b2
Alexkurd <7689609+Alexkurd@users.noreply.github.com>
2021-05-19 14:34:35 +0300

    Add METHOD_PATCH

--- wa-system/file/waNet.class.php
+++ wa-system/file/waNet.class.php
@@ -32,6 +32,7 @@ class waNet
 
     const METHOD_GET = 'GET';
     const METHOD_POST = 'POST';
+    const METHOD_PATCH = 'PATCH';
     const METHOD_PUT = 'PUT';
     const METHOD_DELETE = 'DELETE';
 
