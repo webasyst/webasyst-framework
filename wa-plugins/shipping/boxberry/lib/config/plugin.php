62df5ad34d59e4bec3bc543b8cf2b6146ac455b2
Leonix <leonix@motd.ru>
2021-05-24 17:07:23 +0300

    wa-plugins/shipping/boxberry v.1.1.4   * Добавили в список доступных для выбора регионов России Ханты-Мансийский автономный округ.   * Исправили скачивание файла с этикеткой отправления со страницы просмотра заказа,     если панель управления магазина открыта по протоколу HTTP (без использования SSL-сертификата).

--- wa-plugins/shipping/boxberry/lib/config/plugin.php
+++ wa-plugins/shipping/boxberry/lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'description'      => 'Real-time shipping quotes with <a href="https://boxberry.ru/en/">Boxberry</a> service.',
     'icon'             => 'img/boxberry16.png',
     'logo'             => 'img/boxberry60x32.png',
-    'version'          => '1.1.3',
+    'version'          => '1.1.4',
     'vendor'           => 'webasyst',
     'services_by_type' => true,
 );
