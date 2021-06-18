c5edf4df65841c16534a7c433197dbe4f1fe9506
Leonid Vakulenko <leonix@motd.ru>
2021-06-18 11:13:32 +0300

    UI app v.1.1.0: updated to support features of Webasyst Framework v.2.2.0

--- wa-apps/ui/lib/config/app.php
+++ wa-apps/ui/lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name'    => 'UI',
     'icon'    => 'img/ui.svg',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'vendor'  => 'Webasyst',
     'ui'      => '1.3,2.0',
 );
