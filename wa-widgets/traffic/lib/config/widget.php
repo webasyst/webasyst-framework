0a9eb57accacedfd1c72cd136d6449b717ecf79c
Leonix <leonix@motd.ru>
2021-06-24 16:19:03 +0300

    webasyst 2.0 widgets style update

--- wa-widgets/traffic/lib/config/widget.php
+++ wa-widgets/traffic/lib/config/widget.php
@@ -1,10 +1,9 @@
 <?php
-
 return array(
     'name' => /*_w*/('Traffic'),
     'size' => array('2x2', '2x1', '1x1'),
     'img' => 'img/traffic.png',
     'locale' => 'ru_RU',
-    'version' => '1.0',
+    'version' => '1.1.1',
     'vendor' => 'webasyst',
 );
--- wa-widgets/weather/lib/config/widget.php
+++ wa-widgets/weather/lib/config/widget.php
@@ -3,6 +3,6 @@ return array(
     'name' => /*_w*/('Weather'),
     'size' => array('2x2', '1x1', '2x1'),
     'img' => 'img/weather.png',
-    'version' => '1.3.1',
+    'version' => '1.4.1',
     'vendor' => 'webasyst',
 );
