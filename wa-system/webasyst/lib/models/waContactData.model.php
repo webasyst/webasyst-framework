8ed66f88ecffd4608c25e0674ba737ee6d0a9b67
Anton <info@ensostudio.ru>
2021-11-09 08:08:07 +0300

    Fix waContactDataModel::getPhones() (issue #342)

--- wa-system/webasyst/lib/models/waContactData.model.php
+++ wa-system/webasyst/lib/models/waContactData.model.php
@@ -249,9 +249,13 @@ class waContactDataModel extends waModel
         return $this->getContactIdByFieldValue('phone', $phone);
     }
 
+    /**
+     * @param int $contact_id
+     * @return array[]
+     */
     public function getPhones($contact_id)
     {
-        $sql = "SELECT `values`, ext, status FROM ".$this->table." WHERE contact_id = i:id AND `field` = 'phone' ORDER BY sort";
+        $sql = "SELECT value, ext, status FROM ".$this->table." WHERE contact_id = i:id AND field = 'phone' ORDER BY sort";
         return $this->query($sql, array('id' => $contact_id))->fetchAll();
     }
 
