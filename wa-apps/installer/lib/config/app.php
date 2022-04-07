ff69bf554b90dbf592e21cfb627c1a46434feab3
Leonix <leonix@motd.ru>
2022-04-07 11:51:06 +0300

    Webasyst Framework v.2.5.0   * See full changelog here:     https://www.webasyst.com/platform/changelog/     https://www.webasyst.ru/platform/updates/

--- wa-apps/installer/lib/config/app.php
+++ wa-apps/installer/lib/config/app.php
@@ -4,7 +4,7 @@ return array(
     'description' => 'Install new apps from the Webasyst Store',
     'icon'        => 'img/installer.svg',
     'mobile'      => false,
-    'version'     => '2.5.0',
+    'version'     => '2.5.0', 
     'critical'    => '2.5.0',
     'system'      => true,
     'vendor'      => 'webasyst',
