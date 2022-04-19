1937a8a6b1c296c503abe8e9ead02b86a73b320c
Leonix <leonix@motd.ru>
2022-04-19 18:09:49 +0300

    Webasyst Framework v.2.5.1   * Fixed functioning of reviews-adding widgets in the 2.0 interface mode.   * Several minor improvements in the Webasyst 2 design system.

--- wa-system/webasyst/lib/config/app.php
+++ wa-system/webasyst/lib/config/app.php
@@ -3,8 +3,8 @@
 return array(
     'name'         => 'Webasyst',
     'prefix'       => 'webasyst',
-    'version'      => '2.5.0',
-    'critical'     => '2.5.0',
+    'version'      => '2.5.1',
+    'critical'     => '2.5.1',
     'vendor'       => 'webasyst',
     'csrf'         => true,
     'header_items' => array(
