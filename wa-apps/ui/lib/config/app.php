6744a48103ee14dfb9bd5696026230213403e5fb
Leonix <leonix@motd.ru>
2022-04-19 18:10:48 +0300

    UI app v.2.5.1   * Обновили документацию для компонента waDialog.

--- wa-apps/ui/lib/config/app.php
+++ wa-apps/ui/lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name'    => 'UI',
     'icon'    => 'img/ui.svg',
-    'version' => '2.5.0',
+    'version' => '2.5.1',
     'vendor'  => 'Webasyst',
     'ui'      => '1.3,2.0',
 );
