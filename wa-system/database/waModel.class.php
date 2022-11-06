70f0033c530f274c3a1792013ab008ab74cb6461
Serge Rodovnichenko <sergerod@gmail.com>
2022-11-06 22:40:34 +0300

    countAll() всё же должен возвращать int
    Несмотря на документацию, сейчас `countAll()` возвращает `string`. Пусть уже работает, как записано в документации!

--- wa-system/database/waModel.class.php
+++ wa-system/database/waModel.class.php
@@ -775,9 +775,9 @@ class waModel
      *
      * @return int
      */
-    public function countAll()
+    public function countAll(): int
     {
-        return $this->query("SELECT COUNT(*) FROM ".$this->table)->fetchField();
+        return (int)$this->query("SELECT COUNT(*) FROM ".$this->table)->fetchField();
     }
 
     /**
