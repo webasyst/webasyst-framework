d3526c89da2f71c633b8af029713491feac1e8aa
Leonix <leonix@motd.ru>
2023-07-03 12:17:07 +0300

    Site app v.2.6.1

--- wa-apps/site/lib/config/app.php
+++ wa-apps/site/lib/config/app.php
@@ -5,7 +5,7 @@ return array(
     'icon'       => 'img/site512.png',
     'sash_color' => '#49a2e0',
     'frontend'   => true,
-    'version'    => '2.6.0',
+    'version'    => '2.6.1',
     'critical'   => '2.6.0',
     'vendor'     => 'webasyst',
     'system'     => true,
--- wa-apps/site/lib/config/siteRightConfig.class.php
+++ wa-apps/site/lib/config/siteRightConfig.class.php
@@ -6,6 +6,10 @@ class siteRightConfig extends waRightConfig
     {
         $model = new siteDomainModel();
         $items = $model->select('id, name')->fetchAll('id', true);
+        foreach ($items as &$item) {
+            $item = waIdna::dec($item);
+        }
+        unset($item);
         $this->addItem('domain', _w('Available sites'), 'list', array('items' => $items, 'value' => bindec('11111111')));
     }
 } 
