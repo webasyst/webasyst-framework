688d85fac390d10b88a01913a85b70886bb94a58
Leonix <leonix@motd.ru>
2023-12-27 15:38:20 +0300

    Photos v.2.2.2   * Fixed the functioning of switches in the settings for the Webasyst 2 interface.

--- wa-apps/photos/lib/config/app.php
+++ wa-apps/photos/lib/config/app.php
@@ -10,7 +10,7 @@ return array(
     'plugins' => true,
     'pages' => true,
     'mobile' => true,
-    'version' => '2.2.1',
+    'version' => '2.2.2',
     'vendor' => 'webasyst',
     'my_account' => true,
     'sash_color' => '#af71b3',
