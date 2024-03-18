87011656d2a745ce5ec2308ac01d07e81d7b655e
Leonix <leonix@motd.ru>
2024-03-18 18:04:15 +0300

    wa-plugins/sms/wapattern: test mode that writes all messages to wa-log instead of sending to API

--- wa-plugins/sms/wapattern/lib/wapatternSMS.class.php
+++ wa-plugins/sms/wapattern/lib/wapatternSMS.class.php
@@ -10,6 +10,11 @@ class wapatternSMS extends waSMSAdapter
     public function getControls()
     {
         return array(
+            'testmode' => array(
+                'title'       => 'Enable test mode',
+                'description' => 'Never send via API, write message to wa-log',
+                'control_type' => waHtmlControl::CHECKBOX,
+            ),
             'param_1' => array(
                 'title'       => 'Param name',
                 'description' => 'Param description',
@@ -36,10 +41,16 @@ class wapatternSMS extends waSMSAdapter
 
         $net = new waNet();
         try {
-            $result = $net->query($this->url, $post, waNet::METHOD_POST);
-            $this->log($to, $text, $result);
+            if ($this->getOption('testmode')) {
+                $result = true;
+                $data = sprintf("test mode is enabled.\nMessage text:\n%s", $text);
+                $this->log($to, $text, $data);
+            } else {
+                $result = $net->query($this->url, $post, waNet::METHOD_POST);
+                $this->log($to, $text, $result);
+            }
         } catch (waException $ex) {
-            $data = sprintf("ERROR: %s\nraw response:\n%s", $ex->getMessage(), $net->getResponse(true));
+            $data = sprintf("ERROR: %s\nraw response:\n%s\nMessage text:\n%s", $ex->getMessage(), $net->getResponse(true), $text);
             $this->log($to, $text, $data);
             $result = false;
         }
