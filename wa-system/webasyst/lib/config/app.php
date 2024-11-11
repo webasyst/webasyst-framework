c7d998f16fda9ae9c4d4e813ee5b75b09a3a75e8
Leonid Vakulenko <leonix@motd.ru>
2024-11-11 15:14:21 +0300

    Webasyst Framework v.3.3.1   * Fixed a flaw in the system design editor that occurred after the update to version 3.3.0.

--- wa-system/webasyst/lib/config/app.php
+++ wa-system/webasyst/lib/config/app.php
@@ -3,8 +3,8 @@
 return array(
     'name'         => 'Webasyst',
     'prefix'       => 'webasyst',
-    'version'      => '3.3.0',
-    'critical'     => '3.3.0',
+    'version'      => '3.3.1',
+    'critical'     => '3.3.1',
     'vendor'       => 'webasyst',
     'csrf'         => true,
     'header_items' => array(
