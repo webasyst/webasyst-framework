1201e2413e8daf4a4d6a229782f5415d44adf169
Leonid Vakulenko <leonix@motd.ru>
2025-04-16 11:59:17 +0300

    Webasyst Framework v.3.6.0

--- wa-apps/installer/lib/config/app.php
+++ wa-apps/installer/lib/config/app.php
@@ -4,7 +4,7 @@ return array(
     'description' => 'Install new apps from the Webasyst Store',
     'icon'        => 'img/installer.svg',
     'mobile'      => false,
-    'version'     => '3.6.0', // developer preview
+    'version'     => '3.6.0',
     'critical'    => '3.6.0',
     'system'      => true,
     'vendor'      => 'webasyst',
